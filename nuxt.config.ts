f0830f98466ca65fdc9c13de3085e559642a99da
MehmetAliKOCAL <root.gwyn@hotmail.com>
2024-05-11 13:37:22 +0300

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,43 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  app: {
+    head: {
+      title: 'AHNI QR Generator',
+      meta: [
+        {
+          name: 'description',
+          content: 'QR code generator app made for the TUBITAK competition.',
+        },
+        { property: 'og:url', content: 'https://ahni-qrgenerator.vercel.app' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:title', content: 'AHNI QR Generator' },
+        {
+          property: 'og:description',
+          content: 'QR code generator app made for the TUBITAK competition.',
+        },
+        { property: 'og:image', content: '' },
+        { property: 'twitter:card', content: 'summary_large_image' },
+        { property: 'twitter:domain', content: 'ahni-qrgenerator.vercel.app' },
+        {
+          property: 'twitter:url',
+          content: 'https://ahni-qrgenerator.vercel.app',
+        },
+        { property: 'twitter:title', content: 'AHNI QR Generator' },
+        {
+          property: 'twitter:description',
+          content: 'QR code generator app made for the TUBITAK competition.',
+        },
+        { property: 'twitter:image', content: '' },
+      ],
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+    mollitia, quidem a natus ab qui pariatur animi commodi eum dolores,
+    voluptates nobis debitis ex sit deleniti! Est quia a quod.
+  </div>
+</template>
+<script lang="ts"></script>
